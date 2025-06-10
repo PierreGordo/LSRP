80b05587e3f851e39fef5616e5c4095b421f9438
PierreGordo <peter.gardas@gmail.com>
2025-06-10 10:46:45 +0200

    Done some work on env generation.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+use crate::random_enviroment::Road;
+
 mod random_enviroment;
 
 fn main() {
     let rand_env = random_enviroment::create_enviroment(1, 100, 10, 50);
 
 
+    //Debug print for points
     for point in rand_env.points{
-        print!("{} \n", point);
+        print!("{} \n", point.name);
+    }
+
+    //Debug print for roads
+    for road in rand_env.roads{
+        //                                                      Since road from and road to are both Point structures I need to do it like this
+        print!("Road from: {}, road to: {}, road lenght: {} \n", road.from.name, road.to.name, road.lenght);
+
+    }
+
+    //Debug print for trucks
+    for truck in rand_env.trucks{
+        print!("Truck from: {}, to: {:?}, capacity: {}, stationary: {} \n", truck.origin.name, truck.destination, truck.capacity, truck.stationary)
     }
 
 
--- src/random_enviroment.rs
+++ src/random_enviroment.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::fmt::{write, Display};
 //Using for random enviroment creation
 use rand::prelude::*;
 
@@ -7,25 +7,25 @@ use rand::prelude::*;
 //Truck struct
 pub struct Truck{
     //Not the original origin, but the latest stop
-    origin: Point,
-    destination: Point,
+    pub origin: Point,
+    pub destination: Option<Point>,
     //Not sure about the units yet, just a work in progress
-    capacity: u32,
-    stationary: bool,
+    pub capacity: u32,
+    pub stationary: bool,
 }
 
 
 //Point struct
-#[derive(Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Point{
-    name: u32,
+    pub name: u32,
 }
 
 //Road struct
 pub struct Road{
-    from: Point,
-    to: Point,
-    lenght: u32,
+    pub from: Point,
+    pub to: Point,
+    pub lenght: u32,
 }
 
 //Cargo struct?
@@ -51,7 +51,7 @@ pub struct Enviroment{
 
 //Implementing Truck functions
 impl Truck{
-    fn new(origin: Point, destination: Point, capacity: u32, stationary: bool) -> Truck{
+    fn new(origin: Point, destination: Option<Point>, capacity: u32, stationary: bool) -> Truck{
         Truck { origin: origin, destination: destination, capacity: capacity, stationary: stationary }
     }
 }
@@ -85,17 +85,6 @@ impl Enviroment{
 }
 
 
-//IMPLEMENTING DISPLAY SECTION
-impl Display for Point{
-
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.name)
-    }
-
-}
-
-
-
 //Random enviroment creation to run tests on
 pub fn create_enviroment(min_points: u32, max_points: u32, min_lenght: u32, max_lenght: u32) -> Enviroment{
 
@@ -120,11 +109,23 @@ pub fn create_enviroment(min_points: u32, max_points: u32, min_lenght: u32, max_
     //ALGORITHM: 1) Iterate over all the points.
     //           2) Connect a road from the current point to the next point, except for the last point
 
-    for x in 0..(point_vec.len()-1){
+    for x in 0..(point_vec.len() - 1){
         //Generate the lenght randomly, since its a random enviroment
         road_vec.push(Road::new(point_vec[x].clone(), point_vec[x+1].clone(), rng.random_range(min_lenght..max_lenght)))
     }
 
+    //Generate trucks on points
+    //ALGORITHM: 1) Every time place a truck on the first point
+    //           2) After that flip a coin whether to place a truck on the next point
+
+    
+    for x in 1..(point_vec.len() - 1){
+        if rng.random_bool(0.5){
+            //Later down the line figure out how to properly calculate capacity, for now lets set it to 50
+            truck_vec.push(Truck::new(point_vec[x].clone(), None, 50, true));
+        }
+    }
+    
     Enviroment::new(truck_vec, point_vec, road_vec, cargo_vec)
 
 }
