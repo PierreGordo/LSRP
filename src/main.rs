6b0ffb48cf1879be292be2dd91f203de7e9a2a34
PierreGordo <peter.gardas@gmail.com>
2025-06-10 14:41:24 +0200

    Added ID system and cargo random generation to primitive random enviroment.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-use crate::random_enviroment::Road;
-
 mod random_enviroment;
 
 fn main() {
-    let rand_env = random_enviroment::create_enviroment(1, 100, 10, 50);
+    let rand_env = random_enviroment::create_primitive_enviroment(1, 100, 10, 50, 20);
 
 
     //Debug print for points
     for point in rand_env.points{
-        print!("{} \n", point.name);
+        print!("{} \n", point.id);
     }
 
     //Debug print for roads
     for road in rand_env.roads{
         //                                                      Since road from and road to are both Point structures I need to do it like this
-        print!("Road from: {}, road to: {}, road lenght: {} \n", road.from.name, road.to.name, road.lenght);
+        print!("Road with ID: {} from: {}, road to: {}, road lenght: {} \n", road.id, road.from.id, road.to.id, road.lenght);
 
     }
 
     //Debug print for trucks
     for truck in rand_env.trucks{
-        print!("Truck from: {}, to: {:?}, capacity: {}, stationary: {} \n", truck.origin.name, truck.destination, truck.capacity, truck.stationary)
+        print!("Truck with ID: {} from: {}, to: {:?}, capacity: {}, stationary: {} \n", truck.id, truck.origin.id, truck.destination, truck.capacity, truck.stationary)
+    }
+
+    //Debug print for cargo
+    for cargo in rand_env.cargoes{
+        print!("Cargo with ID: {}, from: {}, to: {}, with capacity usage: {}, is_loaded?: {}, is_stationary?: {} \n", cargo.id, cargo.origin.id, cargo.destination.id, cargo.capacity_usage, cargo.is_loaded, cargo.stationary);
     }
 
 
--- src/random_enviroment.rs
+++ src/random_enviroment.rs
@@ -6,23 +6,29 @@ use rand::prelude::*;
 
 //Truck struct
 pub struct Truck{
+    //Adding ID so I can tell them apart
+    pub id: u32,
     //Not the original origin, but the latest stop
     pub origin: Point,
     pub destination: Option<Point>,
     //Not sure about the units yet, just a work in progress
     pub capacity: u32,
     pub stationary: bool,
+    //Figure out how to connect
+    pub load: Vec<Cargo>,
 }
 
 
 //Point struct
 #[derive(Debug, Clone, PartialEq)]
 pub struct Point{
-    pub name: u32,
+    pub id: u32,
 }
 
 //Road struct
 pub struct Road{
+    //Adding ID so I can tell them apart
+    pub id: u32,
     pub from: Point,
     pub to: Point,
     pub lenght: u32,
@@ -30,12 +36,16 @@ pub struct Road{
 
 //Cargo struct?
 pub struct Cargo{
+    //Adding ID so I can tell them apart
+    pub id: u32,
     //Not the original origin, but the latest stop
-    origin: Point,
-    destination: Point,
+    pub origin: Point,
+    pub destination: Point,
     //How much of the trucks capacity does it use?
-    capacity_usage: u32,
-    stationary: bool,
+    pub capacity_usage: u32,
+    pub stationary: bool,
+    //Whether the cargo is loaded on a truck or not
+    pub is_loaded: bool,
 }
 
 
@@ -51,29 +61,29 @@ pub struct Enviroment{
 
 //Implementing Truck functions
 impl Truck{
-    fn new(origin: Point, destination: Option<Point>, capacity: u32, stationary: bool) -> Truck{
-        Truck { origin: origin, destination: destination, capacity: capacity, stationary: stationary }
+    fn new(id: u32, origin: Point, destination: Option<Point>, capacity: u32, stationary: bool, load: Vec<Cargo>) -> Truck{
+        Truck { id: id, origin: origin, destination: destination, capacity: capacity, stationary: stationary , load: load}
     }
 }
 
 //Implementing Point functions
 impl Point{
-    fn new(name: u32) -> Point{
-        Point { name: name }
+    fn new(id: u32) -> Point{
+        Point { id: id }
     }
 }
 
 //Implementing Road functions
 impl Road{
-    fn new(from: Point, to: Point, lenght: u32) -> Road{
-        Road {from: from, to: to, lenght: lenght}
+    fn new(id: u32, from: Point, to: Point, lenght: u32) -> Road{
+        Road {id: id, from: from, to: to, lenght: lenght}
     }
 }
 
 //Implementing Cargo functions
 impl Cargo{
-    fn new(origin: Point, destination: Point, capacity_usage: u32, stationary: bool) -> Cargo{
-        Cargo {origin: origin, destination: destination, capacity_usage: capacity_usage, stationary: stationary}
+    fn new(id: u32, origin: Point, destination: Point, capacity_usage: u32, stationary: bool, is_loaded: bool) -> Cargo{
+        Cargo {id: id, origin: origin, destination: destination, capacity_usage: capacity_usage, stationary: stationary, is_loaded: is_loaded}
     }
 }
 
@@ -86,7 +96,7 @@ impl Enviroment{
 
 
 //Random enviroment creation to run tests on
-pub fn create_enviroment(min_points: u32, max_points: u32, min_lenght: u32, max_lenght: u32) -> Enviroment{
+pub fn create_primitive_enviroment(min_points: u32, max_points: u32, min_lenght: u32, max_lenght: u32, cargo_count: u32) -> Enviroment{
 
     //First generate points -> random number of points
     let mut rng = rand::rng();
@@ -111,21 +121,38 @@ pub fn create_enviroment(min_points: u32, max_points: u32, min_lenght: u32, max_
 
     for x in 0..(point_vec.len() - 1){
         //Generate the lenght randomly, since its a random enviroment
-        road_vec.push(Road::new(point_vec[x].clone(), point_vec[x+1].clone(), rng.random_range(min_lenght..max_lenght)))
+        road_vec.push(Road::new(x as u32, point_vec[x].clone(), point_vec[x+1].clone(), rng.random_range(min_lenght..max_lenght)))
     }
 
     //Generate trucks on points
     //ALGORITHM: 1) Every time place a truck on the first point
     //           2) After that flip a coin whether to place a truck on the next point
 
+    //Generate first guaranteed truck
+    truck_vec.push(Truck::new(0,  point_vec[0].clone(), None, 50, true, Vec::new()));
     
+    //Generate all subsequent trucs
+    let mut last_id: u32 = 0;
     for x in 1..(point_vec.len() - 1){
         if rng.random_bool(0.5){
-            //Later down the line figure out how to properly calculate capacity, for now lets set it to 50
-            truck_vec.push(Truck::new(point_vec[x].clone(), None, 50, true));
+            last_id += 1;
+            //Later down the line figure out how to properly calculate capacity, for now lets set it to 50                           Hope it can infer the type (Its Vec<Cargo>)
+            truck_vec.push(Truck::new(last_id, point_vec[x].clone(), None, 50, true, Vec::new()));
         }
     }
+
+
+    //Place random cargo on points with random points to go to
+    //ALGORITHM: 1) Pick how much cargo I want to place in the enviroment.
+    //           2) Spread this cargo randomly amongst the points.
+
+    for x in 0..cargo_count{
+        cargo_vec.push(Cargo::new(x, point_vec.choose(&mut rng).unwrap().clone(), point_vec.choose(&mut rng).unwrap().clone(), 10, true, false));
+    }
+
     
+
+
     Enviroment::new(truck_vec, point_vec, road_vec, cargo_vec)
 
 }
